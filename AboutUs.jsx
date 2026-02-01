486c7e9fcf456193a88c5684e4db6f510d7182e2
dilipmettur <dilipmettur@gmail.com>
2026-02-01 10:09:46 +0530

    Add AboutUs component

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+      <p>Welcome to Paradise Nursery, where green dreams come true!</p>
+      <p>Our mission is to provide a wide range of high-quality plants that enhance your surroundings and contribute to a healthier lifestyle.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
